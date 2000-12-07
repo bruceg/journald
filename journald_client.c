1b81f96b8ade7463cfb20461d056367d91800f52
Bruce Guenter <bruce@untroubled.org>
2000-12-07 22:50:30 +0000

    Rewrote the client library to handle output buffering.

--- journald_client.c
+++ journald_client.c
@@ -11,50 +11,81 @@
 		      + strlen ((ptr)->sun_path))
 #endif
 
-static int socket_open(const char* path, const char* ident)
+static int jflush(journald_client* j)
+{
+  long length;
+  char* ptr;
+  long wr;
+  length = j->bufpos;
+  ptr = j->buf;
+  while (length > 0) {
+    wr = write(j->fd, ptr, length);
+    if (wr == -1) return 0;
+    length -= wr;
+    ptr += wr;
+  }
+  j->bufpos = 0;
+  return 1;
+}
+
+static int jwrite(journald_client* j, const char* data, long size)
+{
+  long length;
+  while (size + j->bufpos >= JOURNALD_BUFSIZE) {
+    length = JOURNALD_BUFSIZE - j->bufpos;
+    memcpy(j->buf+j->bufpos, data, length);
+    data += length;
+    size -= length;
+    j->bufpos += length;
+    if (jflush(j) == -1) return 0;
+  }
+  memcpy(j->buf+j->bufpos, data, size);
+  j->bufpos += size;
+  return 1;
+}
+
+static journald_client* socket_open(const char* path, const char* ident)
 {
   size_t size;
   struct sockaddr_un* saddr;
   int fd;
-  size_t wr;
+  journald_client* j;
   
   fd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (fd == -1) return -1;
+  if (fd == -1) return 0;
   size = sizeof(struct sockaddr_un) + strlen(path) + 1;
   saddr = (struct sockaddr_un*)malloc(size);
   saddr->sun_family = AF_UNIX;
   strcpy(saddr->sun_path, path);
-  if (connect(fd, (struct sockaddr*)saddr, SUN_LEN(saddr)) == -1) return -1;
+  if (connect(fd, (struct sockaddr*)saddr, SUN_LEN(saddr)) == -1) return 0;
   free(saddr);
 
-  size = strlen(ident) + 1;
-  while (size) {
-    wr = write(fd, ident, size);
-    if (!wr || wr == -1) {
-      close(fd);
-      return -1;
-    }
-    size -= wr;
-    ident += wr;
+  j = malloc(sizeof(journald_client));
+  memset(j, 0, sizeof(journald_client));
+  j->fd = fd;
+  
+  if (!jwrite(j, ident, strlen(ident)+1)) {
+    free(j);
+    j = 0;
   }
-
-  return fd;
+  return j;
 }
 
-int journald_open(const char* path, const char* ident)
+journald_client* journald_open(const char* path, const char* ident)
 {
-  int fd;
+  journald_client* j;
   
-  if ((fd = socket_open(path, ident)) == -1) return -1;
+  if (!(j = socket_open(path, ident))) return 0;
   
-  if (write(fd, "", 1) != 1) {
-    close(fd);
-    return -1;
+  if (!jwrite(j, "", 1)) {
+    close(j->fd);
+    free(j);
+    j = 0;
   }
-  return fd;
+  return j;
 }
 
-int journald_write(int fd, const char* data, long length)
+int journald_write(journald_client* j, const char* data, long length)
 {
   char lenstr[30];
   char* ptr;
@@ -68,36 +99,37 @@ int journald_write(int fd, const char* data, long length)
     *ptr-- = (tmp % 10) + '0';
   ++ptr;
   
-  if (write(fd, ptr, lenstr+30-ptr) != lenstr+30-ptr) return -1;
+  if (!jwrite(j, ptr, lenstr+30-ptr)) return 0;
 
-  for (total = 0; length > 0; total += wr, data += wr, length -= wr) {
-    wr = write(fd, data, length);
-    if (!wr || wr == -1) return -1;
-  }
-  return total;
+  for (total = 0; length > 0; total += wr, data += wr, length -= wr)
+    if (!jwrite(j, data, length)) return 0;
+
+  return 1;
 }
 
-static int read_status_close(int fd)
+static int read_status_close(journald_client* j)
 {
   char tmp[1];
   
-  if (read(fd, tmp, 1) != 1) return -1;
-  close(fd);
-  return tmp[0] ? 0 : -1;
+  if (!jflush(j)) return 0;
+  if (read(j->fd, tmp, 1) != 1) return 0;
+  close(j->fd);
+  free(j);
+  return tmp[0];
 }
 
-int journald_close(int fd)
+int journald_close(journald_client* j)
 {
-  if (write(fd, "", 1) != 1) return -1;
-  return read_status_close(fd);
+  if (!jwrite(j, "", 1)) return 0;
+  return read_status_close(j);
 }
 
 int journald_oneshot(const char* path, const  char* ident,
 		     const char* data, long length)
 {
-  int fd;
+  journald_client* j;
 
-  if ((fd = socket_open(path, ident)) == -1) return -1;
-  if (journald_write(fd, data, length) != length) return -1;
-  return read_status_close(fd);
+  if (!(j = socket_open(path, ident))) return 0;
+  if (!journald_write(j, data, length)) return 0;
+  return read_status_close(j);
 }
--- journald_client.h
+++ journald_client.h
@@ -1,9 +1,18 @@
 #ifndef JOURNALD__CLIENT__H__
 #define JOURNALD__CLIENT__H__
 
-int journald_open(const char* path, const char* ident);
-int journald_write(int fd, const char* data, long length);
-int journald_close(int fd);
+#define JOURNALD_BUFSIZE 4096
+struct journald_client
+{
+  int fd;
+  int bufpos;
+  char buf[JOURNALD_BUFSIZE];
+};
+typedef struct journald_client journald_client;
+
+journald_client* journald_open(const char* path, const char* ident);
+int journald_write(journald_client* j, const char* data, long length);
+int journald_close(journald_client* j);
 int journald_oneshot(const char* path, const char* ident,
 		     const char* data, long length);
 
