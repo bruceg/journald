b16a790d7dcbbf7e7493e0d7ead7c6dd3e7f6681
Bruce Guenter <bruce@untroubled.org>
2000-12-07 23:27:38 +0000

    Read the records to journal from standard input instead of from the command line.

--- testclient.c
+++ testclient.c
@@ -1,15 +1,21 @@
+#include <unistd.h>
 #include "journald_client.h"
 
 int main(int argc, char* argv[])
 {
-  int fd;
-  int i;
-  if (argc < 4) return 1;
-  if (argc == 4) return journald_oneshot(argv[1], argv[2],
-					 argv[3], strlen(argv[3]));
-  fd = journald_open(argv[1], argv[2]);
-  if (fd == -1) return 2;
-  for (i = 3; i < argc; ++i)
-    if (journald_write(fd, argv[i], strlen(argv[i])) == -1) return 3;
-  return journald_close(fd);
+  journald_client* j;
+  char buf[4096];
+  long rd;
+
+  if (argc != 3) return 1;
+  j = journald_open(argv[1], argv[2]);
+  if (!j) return 2;
+  for(;;) {
+    rd = read(0, buf, 4096);
+    if (rd == -1) return 3;
+    if (!rd) break;
+    if (!journald_write(j, buf, rd)) return 4;
+  }
+  if (!journald_close(j)) return 5;
+  return 0;
 }
