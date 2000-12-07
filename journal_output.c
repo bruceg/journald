a03d62b67f178a23b6f8723dfee11af178b3c12c
Bruce Guenter <bruce@untroubled.org>
2000-12-07 23:29:43 +0000

    Write records out with the new file format.

--- journal_output.c
+++ journal_output.c
@@ -7,103 +7,99 @@
 
 static int fdout = -1;
 
-static char* itoa(long i)
+int open_journal(const char* path)
 {
-  static char buf[30];
-  char* ptr = buf + sizeof buf - 1;
-  *ptr-- = 0;
-  while(i > 0) {
-    *ptr-- = (i % 10) + '0';
-    i /= 10;
-  }
-  return ptr + 1;
+  if (chdir(path)) return 0;
+  fdout = open("current", O_WRONLY|O_CREAT|O_TRUNC, 0644);
+  return fdout != -1;
+}
+
+static void ulong2bytes(unsigned long v, char bytes[4])
+{
+  bytes[3] = v & 0xff; v >>= 8;
+  bytes[2] = v & 0xff; v >>= 8;
+  bytes[1] = v & 0xff; v >>= 8;
+  bytes[0] = v;
+}
+
+static CRC32 set_iov(struct iovec* iov, char* data, long size, CRC32 crc)
+{
+  iov->iov_base = data;
+  iov->iov_len = size;
+  return CRC32_block(crc, data, size);
 }
 
 static char saved_type = 0;
 static long bytes_written = 0;
 
-// TODO:
-// - Open the output file
-// - Temporarily mark the first part of a transaction
-// - Rotate the output file after writing
-// - Partial writes
+/*
+  TODO:
+  - Temporarily mark the first part of a transaction
+  - Rotate the output file after writing
+  - Partial writes
+*/
 
 int write_record(connection* con, int final, int abort)
 {
   char type[1];
-  struct iovec iov[5];
-  char* lengthstr;
+  struct iovec iov[6];
+  unsigned char ilen[4];
+  unsigned char rlen[4];
+  unsigned char crcbytes[4];
   CRC32 crc;
   long wr;
   
   if (abort) {
-    type[0] = 'A';
     con->buf_length = 0;
+    if (!con->not_first) return 1;
+    type[0] = 'A';
   }
-  else if (final) {
-    if (!con->buf_offset)
-      type[0] = 'O';
-    else
-      type[0] = 'E';
-  }
-  else {
-    if (!con->buf_offset)
-      type[0] = 'S';
-    else
-      type[0] = 'C';
-  }
-  
-  lengthstr = itoa(con->buf_length);
+  else
+    type[0] = final ?
+      (con->not_first ? 'E' : 'O') : (con->not_first ? 'C' : 'S');
+
+  ulong2bytes(con->ident_len, ilen);
+  ulong2bytes(con->buf_length, rlen);
   crc = CRC32init;
-  crc = CRC32_block(crc, type, 1);
-  iov[0].iov_base = type;
-  iov[0].iov_len = 1;
-
-  crc = CRC32_block(crc, con->ident, con->ident_len+1);
-  iov[1].iov_base = con->ident;
-  iov[1].iov_len = con->ident_len+1;
-
-  crc = CRC32_block(crc, lengthstr, strlen(lengthstr)+1);
-  iov[2].iov_base = lengthstr;
-  iov[2].iov_len = strlen(lengthstr)+1;
-
-  crc = CRC32_block(crc, con->buf, con->buf_length);
-  iov[3].iov_base = con->buf;
-  iov[3].iov_len = con->buf_length;
-
-  iov[4].iov_base = &crc;
-  iov[4].iov_len = 4;
+  crc = set_iov(iov+0, type, 1, crc);
+  crc = set_iov(iov+1, ilen, 4, crc);
+  crc = set_iov(iov+2, rlen, 4, crc);
+  crc = set_iov(iov+3, con->ident, con->ident_len, crc);
+  crc = set_iov(iov+4, con->buf, con->buf_length, crc);
+  ulong2bytes(crc, crcbytes);
+  set_iov(iov+5, crcbytes, 4, crc);
   
   if (!saved_type) {
     saved_type = type[0];
     type[0] = 0;
   }
-  wr = writev(fdout, iov, 5);
-  if (!wr || wr == -1) return -1;
+  if ((wr = writev(fdout, iov, 6)) != 1+4+4+con->ident_len+con->buf_length+4)
+    return 0;
   bytes_written += wr;
-  con->buf_offset += con->buf_length;
-  return 0;
+  con->not_first = 1;
+  con->buf_length = 0;
+  return 1;
 }
 
 int sync_records(void)
 {
   char type[1];
-  if (!saved_type) return 0;
+  if (!saved_type) return 1;
 
   /* First, append an empty record marker and sync the data */
   type[0] = 0;
-  if (write(fdout, type, 1) != 1) return -1;
-  if (fdatasync(fdout)) return -1;
+  if (write(fdout, type, 1) != 1) return 0;
+  if (fdatasync(fdout)) return 0;
 
   /* Then restore the original starting record type byte and sync again */
-  if (lseek(fdout, -(bytes_written+1), SEEK_CUR) == -1) return -1;
+  if (lseek(fdout, -(bytes_written+1), SEEK_CUR) == -1) return 0;
   type[0] = saved_type;
-  if (write(fdout, type, 1) != 1) return -1;
-  if (fdatasync(fdout)) return -1;
+  if (write(fdout, type, 1) != 1) return 0;
+  if (fdatasync(fdout)) return 0;
   saved_type = 0;
-  bytes_written = 0;
 
   /* Finally, seek back to where this routine started. */
-  if (lseek(fdout, bytes_written, SEEK_CUR) == -1) return -1;
-  return 0;
+  if (lseek(fdout, bytes_written-1, SEEK_CUR) == -1) return 0;
+  bytes_written = 0;
+  return 1;
 }
