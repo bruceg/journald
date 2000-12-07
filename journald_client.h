788728f5a554ad99e7d99808f867ff5ef43bfb25
Bruce Guenter <bruce@untroubled.org>
2000-12-07 23:26:46 +0000

    Make all length fields unsigned.

--- journald_client.h
+++ journald_client.h
@@ -5,15 +5,15 @@
 struct journald_client
 {
   int fd;
-  int bufpos;
+  unsigned bufpos;
   char buf[JOURNALD_BUFSIZE];
 };
 typedef struct journald_client journald_client;
 
 journald_client* journald_open(const char* path, const char* ident);
-int journald_write(journald_client* j, const char* data, long length);
+int journald_write(journald_client* j, const char* data, unsigned long length);
 int journald_close(journald_client* j);
 int journald_oneshot(const char* path, const char* ident,
-		     const char* data, long length);
+		     const char* data, unsigned long length);
 
 #endif
