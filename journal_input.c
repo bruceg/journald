f6541f8628f7e87c76f4e6c53f66d3e1766dd954
Bruce Guenter <bruce@untroubled.org>
2000-12-07 23:30:04 +0000

    Rewrote the state machine to handle the new (simplified) protocol.

--- journal_input.c
+++ journal_input.c
@@ -9,105 +9,82 @@
 /*
   Per-connection algorithm:
   - Accept connection
-  - Read in identifier string (state 0: reading identifier)
-  - Read in record length (state 1: reading initial length)
-  - If length is not zero:
-    - read in record (state 2: reading only record)
+  - Read in identifier length (state 0: reading identifier length)
+  - Read in identifier string (state 1: reading identifier)
+  - read in record length (state 2: reading next record length)
+  - while length is not zero:
+    - read in record (state 3: reading record)
     - write out journal record
-  - Otherwise:
-    - read in record length (state 3: reading next record length)
-    - while length is not zero:
-      - read in record (state 4: reading record)
-      - write out journal record
-      - read in next record length (state 3: reading next record length)
+    - read in next record length (state 2: reading next record length)
   - Send OK code (state -1: sending response)
   - Close connection
 */
 
 int needs_fsync = 0;
 
-static int read_identifier(connection* con, char byte)
+static int read_ident_length(connection* con, char byte)
 {
-  fflush(stdout);
-  con->ident[con->ident_len++] = byte;
-  return byte ? 0 : 1;
+  con->ident_len <<= 8;
+  con->ident_len |= (unsigned char)byte;
+  con->count++;
+  if (con->count == 4) {
+    con->count = 0;
+    return 1;
+  }
+  else
+    return 0;
 }
 
-static int read_init_length(connection* con, char byte)
+static int read_ident(connection* con, char byte)
 {
-  if (byte) {
-    if (byte >= '0' && byte <= '9') {
-      con->length = con->length*10 + byte - '0';
-      return 1;
-    }
-    else
-      return -1;
-  }
-  else if (con->length) {
-    con->buf_offset = 0;
-    con->buf_length = 0;
+  con->ident[con->count++] = byte;
+  if (con->count == con->ident_len) {
+    con->count = 0;
+    con->length = 0;
     return 2;
   }
   else
-    return 3;
+    return 1;
 }
 
-static int read_only_record(connection* con, char byte)
+static int read_record_length(connection* con, char byte)
 {
-  con->buf[con->buf_length++] = byte;
-  if (con->buf_offset + con->buf_length == con->length) {
-    con->ok = !write_record(con, 1, 0);
-    if (con->ok) needs_fsync = 1;
-    return -1;
-  }
-  if (con->buf_length == CBUFSIZE) {
-    if (write_record(con, 0, 0))
-      return -1;
-  }
-  return 2;
-}
-
-static int read_next_length(connection* con, char byte)
-{
-  if (byte) {
-    if (byte >= '0' && byte <= '9') {
-      con->length = con->length*10 + byte - '0';
+  con->length <<= 8;
+  con->length |= (unsigned char)byte;
+  con->count++;
+  if (con->count == 4) {
+    if (con->length) {
+      con->count = 0;
       return 3;
     }
     else {
-      write_record(con, 0, 1);
+      con->ok = write_record(con, 1, 0);
+      if (con->ok)
+	needs_fsync = 1;
       return -1;
     }
   }
-  else {
-    if (!con->length) {
-      con->ok = !write_record(con, 1, 0);
-      if (con->ok) needs_fsync = 1;
-      return -1;
-    }
-    else {
-      con->buf_offset = 0;
-      con->buf_length = 0;
-      return 4;
-    }
-  }
+  else
+    return 2;
 }
 
-static int read_next_record(connection* con, char byte)
+static int read_record(connection* con, char byte)
 {
   con->buf[con->buf_length++] = byte;
-  if (con->buf_offset + con->buf_length == con->length) {
-    if (write_record(con, 0, 0))
-      return -1;
-    con->length = 0;
-    return 3;
-  }
+  con->count++;
   if (con->buf_length == CBUFSIZE) {
-    if (write_record(con, 0, 0))
+    if (!write_record(con, 0, 0))
       return -1;
   }
-  return 4;
+  if (con->count == con->length) {
+    con->count = 0;
+    con->length = 0;
+    return 2;
+  }
+  return 3;
 }
+
+#define DEBUG
 
 void handle_data(connection* con, char* data, long size)
 {
@@ -118,18 +95,18 @@ void handle_data(connection* con, char* data, long size)
   else {
     for (state = con->state; size > 0; ++data, --size) {
 #ifdef DEBUG
-      printf("state=%d byte=%d(%c) length=%ld ",
-	     state, *data, *data, con->length);
+      printf("state=%d byte=%d(%c) length=%ld buf_length=%ld count=%d ",
+	     state, *data, *data, con->length, con->buf_length, con->count);
 #endif
       switch (state) {
-      case 0: state = read_identifier(con, *data); break;
-      case 1: state = read_init_length(con, *data); break;
-      case 2: state = read_only_record(con, *data); break;
-      case 3: state = read_next_length(con, *data); break;
-      case 4: state = read_next_record(con, *data); break;
+      case 0: state = read_ident_length(con, *data); break;
+      case 1: state = read_ident(con, *data); break;
+      case 2: state = read_record_length(con, *data); break;
+      case 3: state = read_record(con, *data); break;
+      default: die("Invalid state in handle_data");
       }
 #ifdef DEBUG
-      printf("next state=%d\n", state);
+      printf("next=%d\n", state);
 #endif
     }
     con->state = state;
