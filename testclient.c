c8537e8e1ac09a78dc086953737b66281f34158d
Bruce Guenter <bruce@untroubled.org>
2000-12-07 06:32:19 +0000

    Take the journal socket path as an argument. Handle a single output record as a "oneshot" call.

--- testclient.c
+++ testclient.c
@@ -4,9 +4,12 @@ int main(int argc, char* argv[])
 {
   int fd;
   int i;
-  if (argc < 3) return 1;
-  fd = journald_open("/tmp/.journald", argv[1]);
-  for (i = 2; i < argc; ++i)
-    journald_write(fd, argv[i], strlen(argv[i]));
+  if (argc < 4) return 1;
+  if (argc == 4) return journald_oneshot(argv[1], argv[2],
+					 argv[3], strlen(argv[3]));
+  fd = journald_open(argv[1], argv[2]);
+  if (fd == -1) return 2;
+  for (i = 3; i < argc; ++i)
+    if (journald_write(fd, argv[i], strlen(argv[i])) == -1) return 3;
   return journald_close(fd);
 }
