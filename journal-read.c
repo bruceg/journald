d4a5cf3ec9e02401c42f2cf90790d3a63dd63b62
Bruce Guenter <bruce@untroubled.org>
2000-12-14 09:48:07 +0000

    Modified to optionally remove journal files after all the streams in that file are completed.

--- journal-read.c
+++ journal-read.c
@@ -24,7 +24,44 @@
 #include <unistd.h>
 #include "hash.h"
 
-void die(const char* msg)
+static const char* program = "journal-read";
+
+#define MSG0(S) do{ printf("%s: %s\n", program, S); }while(0)
+#define MSG1(S,A) do{ printf("%s: " S "\n", program, A); }while(0)
+#define MSG2(S,A,B) do{ printf("%s: " S "\n", program, A, B); }while(0)
+#define MSG3(S,A,B,C) do{ printf("%s: " S "\n", program, A, B, C); }while(0)
+
+struct dirent_node
+{
+  struct dirent entry;
+  unsigned long use_count;
+  struct dirent_node* next;
+};
+typedef struct dirent_node dirent_node;
+
+struct handler
+{
+  unsigned long stream;
+  unsigned long offset;
+  unsigned long start_offset;
+  int fd;
+  unsigned long identlen;
+  char* ident;
+  struct handler* next;
+  dirent_node* entry;
+};
+typedef struct handler handler;
+
+static char** opt_argv;
+static int opt_argc;
+static int opt_unlink;
+
+static handler* handlers;
+
+static dirent_node* direntries;
+static dirent_node* curr_entry;
+
+static void die(const char* msg)
 {
   perror(msg);
   exit(1);
@@ -35,56 +72,47 @@ static unsigned long bytes2ulong(const unsigned char bytes[4])
   return (bytes[0]<<24) | (bytes[1]<<16) | (bytes[2]<<8) | bytes[3];
 }
 
-struct dirent_node
+static int cmp_dirent_node(const void* a, const void* b)
 {
-  struct dirent entry;
-  struct dirent_node* next;
-};
-typedef struct dirent_node dirent_node;
-
-static int cmp_dirent(const void* a, const void* b)
-{
-  const struct dirent* da = (struct dirent*)a;
-  const struct dirent* db = (struct dirent*)b;
-  return strcmp(da->d_name, db->d_name);
+  const dirent_node* da = *(dirent_node**)a;
+  const dirent_node* db = *(dirent_node**)b;
+  return strcmp(da->entry.d_name, db->entry.d_name);
 }
 
-static struct dirent* read_directory(void)
+static void read_directory(void)
 {
   DIR* dir;
   struct dirent* de;
-  dirent_node* head;
-  dirent_node* tail;
   dirent_node* n;
+  dirent_node* list;
+  dirent_node** tmp;
   unsigned count;
   unsigned i;
   
   dir = opendir(".");
   if (!dir) die("opendir");
-  head = tail = 0;
+  list = 0;
   count = 0;
   while ((de = readdir(dir)) != 0) {
     if (de->d_name[0] == '.') continue;
     n = malloc(sizeof(dirent_node));
     n->entry = *de;
-    n->next = 0;
-    if (tail)
-      tail->next = n;
-    else
-      head = n;
-    tail = n;
+    n->use_count = 0;
+    n->next = list;
+    list = n;
     ++count;
   }
   closedir(dir);
-  de = calloc(count+1, sizeof(struct dirent));
-  for (i = 0; head && i < count; i++, head = n) {
-    n = head->next;
-    de[i] = head->entry;
-    free(head);
-  }
-  de[count].d_name[0] = 0;
-  qsort(de, count, sizeof(struct dirent), cmp_dirent);
-  return de;
+
+  tmp = calloc(count, sizeof(dirent_node*));
+  for (i = 0; list && i < count; i++, list = list->next)
+    tmp[i] = list;
+  qsort(tmp, count, sizeof(dirent_node*), cmp_dirent_node);
+  direntries = tmp[0];
+  for (i = 0; i < count-1; i++)
+    tmp[i]->next = tmp[i+1];
+  tmp[i]->next = 0;
+  free(tmp);
 }
 
 static char* ulongtoa(unsigned long i)
@@ -100,21 +128,6 @@ static char* ulongtoa(unsigned long i)
   }
   return ptr+1;
 }
-
-static char** opt_argv;
-static int opt_argc;
-
-struct handler
-{
-  unsigned long stream;
-  unsigned long offset;
-  unsigned long start_offset;
-  int fd;
-  unsigned long identlen;
-  char* ident;
-  struct handler* next;
-};
-typedef struct handler handler;
 
 static void exec_handler(handler* h)
 {
@@ -135,8 +148,6 @@ static void exec_handler(handler* h)
   if (waitpid(pid, 0, WUNTRACED) != pid) die("waitpid");
 }
 
-static handler* handlers;
-
 static handler* add_handler(unsigned long stream, unsigned long offset,
 			    char* id, unsigned long idlen)
 {
@@ -152,6 +163,8 @@ static handler* add_handler(unsigned long stream, unsigned long offset,
   memcpy(n->ident, id, idlen);
   n->ident[idlen] = 0;
   n->next = handlers;
+  n->entry = curr_entry;
+  n->entry->use_count++;
   handlers = n;
   return n;
 }
@@ -176,6 +189,14 @@ static void del_handler(handler* find)
       else
 	handlers = ptr->next;
       close(ptr->fd);
+      ptr->entry->use_count--;
+      if (opt_unlink) {
+	while (!direntries->use_count && direntries != curr_entry) {
+	  MSG1("Removing '%s'...", direntries->entry.d_name);
+	  unlink(direntries->entry.d_name);
+	  direntries = direntries->next;
+	}
+      }
       free(ptr);
       return;
     }
@@ -194,18 +215,66 @@ static char* alloc_buffer(unsigned long size)
   return buf;
 }
 
+static void handle_record(char type, unsigned long strnum,
+			  unsigned long reclen, const char* buf)
+{
+  handler* h;
+  unsigned long offset;
+  
+  h = find_handler(strnum);
+  if (type == 'A') {
+    if (h)
+      del_handler(h);
+  }
+  else if (type == 'I') {
+    offset = bytes2ulong(buf);
+    if (h) {
+      if (offset != h->offset) {
+	MSG3("Bad offset value for stream #%lu, dropping stream\n"
+	     "  Offset was %lu, should be %lu\n", h->stream,
+	     offset, h->offset);
+	del_handler(h);
+      }
+    }
+    else {
+      MSG2("Start stream #%lu at offset %lu", strnum, offset);
+      add_handler(strnum, offset, (char*)buf+4, reclen-4);
+    }
+  }
+  else {
+    if (type == 'O' || type == 'D' || type == 'E') {
+      if (h) {
+	if (write(h->fd, buf, reclen) != reclen)
+	  die("write to temporary file");
+	h->offset += reclen;
+      }
+      else
+	MSG1("Data record for nonexistant stream #%lu", strnum);
+    }
+    if (type == 'O' || type == 'E') {
+      if (h) {
+	MSG2("End stream #%lu at offset %lu", strnum, h->offset);
+	exec_handler(h);
+	del_handler(h);
+      }
+      else
+	MSG1("End record for nonexistant stream #%lu\n", strnum);
+    }
+  }
+}
+
 static int read_record(int in)
 {
+#undef FAIL
+#define FAIL(ARGS) do{ printf ARGS; return 0; }while(0)
   static unsigned char header[1+4+4+4];
   static char hcmp[HASH_SIZE];
   static HASH_CTX hash;
   char type;
   unsigned long strnum;
   unsigned long recnum;
-  unsigned long offset;
   unsigned long reclen;
   const unsigned char* hdrptr;
-  handler* h;
   char* buf;
   
   if (read(in, header, sizeof header) != sizeof header) return 0;
@@ -216,89 +285,77 @@ static int read_record(int in)
   reclen = bytes2ulong(hdrptr);
   buf = alloc_buffer(reclen+HASH_SIZE);
   if (read(in, buf, reclen+HASH_SIZE) != reclen+HASH_SIZE)
-    return 0;
+    FAIL(("journal-read: Could not read record data.\n"));
   hash_init(&hash);
   hash_update(&hash, header, sizeof header);
   hash_update(&hash, buf, reclen);
   hash_finish(&hash, hcmp);
-  if (memcmp(buf+reclen, hcmp, HASH_SIZE)) return 0;
+  if (memcmp(buf+reclen, hcmp, HASH_SIZE))
+    FAIL(("journal-read: Record data was corrupted.\n"));
 
-  h = find_handler(strnum);
-  if (type == 'A') {
-    if (h)
-      del_handler(h);
-  }
-  else if (type == 'I') {
-    offset = bytes2ulong(buf);
-    if (h) {
-      if (offset != h->offset) {
-	printf("journal-read: "
-	       "Bad offset value for stream #%lu, dropping stream\n"
-	       "  Offset was %lu, should be %lu\n", h->stream,
-	       offset, h->offset);
-	del_handler(h);
-      }
-    }
-    else
-      add_handler(strnum, offset, (char*)buf+4, reclen-4);
-  }
-  else {
-    if (type == 'O' || type == 'D' || type == 'E') {
-      if (h) {
-	if (write(h->fd, buf, reclen) != reclen)
-	  die("write to temporary file");
-	h->offset += reclen;
-      }
-      else
-	printf("journal-read: "
-	       "Data record for nonexistant stream #%lu\n", strnum);
-    }
-    if (type == 'O' || type == 'E') {
-      if (h) {
-	exec_handler(h);
-	del_handler(h);
-      }
-      else
-	printf("journal-read: "
-	       "End record for nonexistant stream #%lu\n", strnum);
-    }
-  }
+  handle_record(type, strnum, reclen, buf);
   return 1;
 }
 
-static void read_journal(const char* filename)
+static void read_journal()
 {
 #undef FAIL
 #define FAIL(MSG) do{fprintf(stderr,"journal-read: " MSG ", skipping\n", filename);return;}while(0)
   static char header[16];
   int in;
+  const char* filename;
+
+  filename = curr_entry->entry.d_name;
   if ((in = open(filename, O_RDONLY)) == -1) FAIL("Could not open '%s'");
   if (read(in, header, sizeof header) != sizeof header)
     FAIL("Could not read header from '%s'");
   if (memcmp(header, "journald", 8)) FAIL("'%s' is not a journald file");
   if (bytes2ulong(header+8) != 1)
     FAIL("'%s' is not a version 1 journald file");
-  printf("File:%s\n", filename);
+  MSG1("Start file '%s'", filename);
   while (read_record(in))
     ;
   close(in);
+  MSG1("End file '%s'", filename);
+}
+
+static void usage(const char* msg)
+{
+  if (msg)
+    fprintf(stderr, "%s: %s\n", "journal-read", msg);
+  fputs("usage: journal-read [-u] directory program [args ...]\n", stderr);
+  exit(1);
+}
+
+static void parse_args(int argc, char* argv[])
+{
+  int opt;
+  int i;
+  while ((opt = getopt(argc, argv, "u")) != EOF) {
+    switch (opt) {
+    case 'u': opt_unlink = 1; break;
+    default: usage(0);
+    }
+  }
+  argc -= optind;
+  argv += optind;
+  if (argc < 2) usage("Too few command-line arguments");
+  if (chdir(argv[0])) die("chdir");
+  opt_argc = argc - 1;
+  opt_argv = malloc(sizeof(char*) * (opt_argc+3));
+  for (i = 0; i < opt_argc; i++)
+    opt_argv[i] = argv[i+1];
 }
 
 int main(int argc, char* argv[])
 {
-  struct dirent* de;
-  unsigned i;
-  if (argc < 3) {
-    fprintf(stderr, "usage: %s directory program [args ...]\n", argv[0]);
-    return 1;
-  }
-  if (chdir(argv[1])) die("chdir");
-  opt_argc = argc - 2;
-  opt_argv = malloc(sizeof(char*) * (opt_argc+3));
-  for (i = 0; i < opt_argc; i++)
-    opt_argv[i] = argv[i+2];
-  de = read_directory();
-  for (i = 0; de[i].d_name[0]; i++)
-    read_journal(de[i].d_name);
+  handler* h;
+  parse_args(argc, argv);
+  read_directory();
+  for (curr_entry = direntries; curr_entry; curr_entry = curr_entry->next)
+    read_journal();
+  for (h = handlers; h; h = h->next)
+    printf("journal-read: Stream #%lu ID '%s' had no end marker\n",
+	   h->stream, h->ident);
   return 0;
 }
