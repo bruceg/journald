cb8ca9ee7efb52832719fa706c5795bc41a0cb48
Bruce Guenter <bruce@untroubled.org>
2002-09-26 05:40:12 +0000

    Use 64-bit CRC instead of MD4 for the hash code.

--- hash.h
+++ hash.h
@@ -1,12 +1,12 @@
 #ifndef JOURNALD__HASH__H__
 #define JOURNALD__HASH__H__
 
-#include "md4.h"
-#define HASH_SIZE 16
-typedef struct md4_ctx HASH_CTX;
+#include "crc64.h"
+#define HASH_SIZE (sizeof(uint64))
+typedef uint64 HASH_CTX;
 
-#define hash_init(H) do{ md4_init_ctx(H); }while(0)
-#define hash_update(H,B,L) do{ md4_process_bytes(B,L,H); }while(0)
-#define hash_finish(H,BUF) do{ md4_finish_ctx(H,BUF); }while(0)
+#define hash_init(H) do{ *(H) = CRC64INIT; }while(0)
+#define hash_update(H,B,L) do{ *H = crc64_update(*H,B,L); }while(0)
+#define hash_finish(H,BUF) do{ uint64 tmp = ~(*(H)); memcpy(BUF, &tmp, sizeof tmp); }while(0)
 
 #endif
