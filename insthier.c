f1057de27fd2089e4bca4513026b542437af93ad
Bruce Guenter <bruce@untroubled.org>
2002-10-09 10:55:53 +0000

    Removed old journald-WRITER programs.

--- insthier.c
+++ insthier.c
@@ -3,10 +3,8 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, "journald-client",     -1, -1, 0755);
-  c(bin, "journald-fdatasync",  -1, -1, 0755);
-  c(bin, "journald-mmap",       -1, -1, 0755);
-  c(bin, "journald-open-dsync", -1, -1, 0755);
-  c(bin, "journal-dump",        -1, -1, 0755);
-  c(bin, "journal-read",        -1, -1, 0755);
+  c(bin, "journald-client", -1, -1, 0755);
+  c(bin, "journald",        -1, -1, 0755);
+  c(bin, "journal-dump",    -1, -1, 0755);
+  c(bin, "journal-read",    -1, -1, 0755);
 }
